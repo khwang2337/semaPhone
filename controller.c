9af6e2cace1e80913bec25383d363958a3acfd17
khwang1 <jh2933@gmail.com>
2016-12-08 18:19:50 +0000

    stuffs, done with controller, print statements left in, in case

--- controller.c
+++ controller.c
@@ -21,37 +21,41 @@ int main(int args, char * argv[]){
 	int key = ftok("makefile", 192);
 	int test;
 	int fd;
+	int len;
 
-
-	if (strcmp( argv[1], "-c") == 0){
-		printf("%d\n", key);
+	if (strcmp( argv[1], "-c") == 0) {
+		//printf("%d\n", key);
 		shmid = shmget(key, 8, IPC_CREAT | 0644);
-		printf("%d\n",shmid );
+		//printf("%d\n",shmid );
 		semid = semget(key, 1, IPC_CREAT | 0644);
 
 		union semun sem;
 		sem.val = 1;
 		test = semctl(semid, 0, SETVAL, sem);
-		printf("%d\n",test );
+		//printf("%d\n",test );
 
 		fd = open("stories", O_CREAT | O_TRUNC, 0644);
-
-
-	}else if (strcmp(argv[1], "-r") == 0){
+	} 
+	
+	else if (strcmp(argv[1], "-r") == 0) {
 		shmid = shmget(key, 8, 0);
 		semid = semget(key, 1, 0);
 
 		test = semctl(semid, 0, IPC_RMID);
-		printf("%d\n", test);
+		//printf("%d\n", test);
 		struct shmid_ds d;
 		test = shmctl(shmid, IPC_RMID, &d);
-		printf("%d\n", test);
+		//printf("%d\n", test);
 
 		fd = open("stories", O_RDONLY , 0644);
-		char * s;
-		printf("fine\n");
-		read(fd, s, 0);//refine, get length of file
-		printf("%s\n", s);
+		int character;
+		FILE *story;
+		story = fopen("stories", "r");
+		if (story) {
+    		while ( (character = getc(story)) != EOF ) putchar(character);
+    		fclose(story);
+		}
 	}
+	
 	return 0;
 }
--- writer.c
+++ writer.c
@@ -8,6 +8,18 @@
 #include <string.h>
 #include <fcntl.h>
 
-int main(){
+int main() {
+    
+    
+    
 	return 0;
 }
+
+/*
+Will attempt to access the resources using the semaphore
+    Once in, it should display the last line added to the file (the shared memory should contain the size of the last line).
+    Then prompt the use for the next line.
+    Once a new line is added, it should write that to the file, update the shared memory and then release the semaphore
+    
+    Got controller working - KH
+*/
